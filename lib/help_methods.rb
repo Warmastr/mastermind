696472cd4b14ab60585e3330e5f17f65f95082e8
Warmastr <jargyle80@yahoo.com>
2023-03-14 23:42:13 -0500

    storage for game methods

---
+++ lib/help_methods.rb
@@ -0,0 +1,20 @@
+# This method will be used to determine the number of red and white pegs to display
+# it takes in the temporary array (the 4 digit slice of array based on the current round) and the computer's code
+# it will then compare the two arrays and will determine the number of red and white pegs to display for each round
+# this method is called.
+def game_hint(temp_array, computer_code)
+  zip = computer_code.zip(temp_array)
+  zip = zip.each do |ele|
+    ele.sort!
+  end
+
+  zip = zip.uniq!
+  difference = temp_array.map.each_with_index { |num, index| num == computer_code[index] }
+  difference = difference.each_index.select { |i| difference[i] }
+  red_pegs = difference.count
+  intersection = temp_array & computer_code
+  white_pegs = intersection.count - red_pegs
+  red_pegs, white_pegs = [red_pegs, white_pegs]
+end
+puts "red pegs = #{game_hint(temp_array, computer_code)[0]}"
+puts "white pegs = #{game_hint(temp_array, computer_code)[1]}"
