a0f08c793f8e822931743f4a2f97f1e9e0573f5c
Warmastr <jargyle80@yahoo.com>
2023-03-16 14:30:58 -0500

    added game_setup method to establish the game

--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,18 @@ require_relative "./display"
 require_relative "./game_board"
 require "pry-byebug"
 class Game
-
+  using ColorString
   include Display
+  attr_reader :game_board, :round_number
 
   def initialize
     @game_board = Board.new
-    puts @game_board
-    @round_number = 1
+    @round_number = 10
+  end
+
+  def play
+    game_setup
+
   end
 
   def computer_code
@@ -18,18 +23,65 @@ class Game
     code = combos.sample
   end
 
-  def Round
-    display.clear
-    display.title
-    puts "Round #{@round_number}:\nUsing the legend above: "
-    puts "Please enter your guess:"
-    player_input
-    @round_number += 1
+  def game_setup
+    clear
+    # adjusts the formatting for the title based on the round number
+    if round_number >= 0 && round_number < 10
+      title
+    else
+      title_round_10
+    end 
+    
+    rules
+    puts "Would you like to be the codebreaker or the codemaker? Please enter [1] or type 'breaker' or enter [2] or type 'maker'."
+    game_mode = gets.chomp
+    if game_mode == "breaker" || game_mode == 1.to_s
+      code_breaker
+    elsif game_mode == "maker" || game_mode == 2.to_s
+      code_maker
+    else
+      puts "Invalid input. Please try again. You can enter 'breaker' or 'maker'."
+      game_setup
+    end
+  end
+
+  def code_breaker
+    clear
+    title
+    puts "You are the codebreaker. The computer will generate a random code. Your job is to guess the code in 12 rounds or less. You will be given feedback after each round. Good luck!"
+    game_board.show
+    
+    game_code = computer_code
+    p game_code # remove this
+    temp_guess = player_input
+    game_hint(temp_guess, game_code)
+    
+    puts "red pegs = #{game_hint(temp_guess, game_code)[0]}"
+    puts "white pegs = #{game_hint(temp_guess, game_code)[1]}"
   end
 
   def player_input
-    puts "Please enter your four-digit guess: You can use the numbers 1-6 that represent the colors in the legend above."
+    puts "\nPlease enter your four-digit guess: You can use the numbers 1-6 that represent the colors in the legend above."
     player_choice = gets.chomp
-    player_choice = player_choice.split("")
+    player_choice = player_choice.split("").map(&:to_i)
   end
+end
+
+def game_hint(guess, c_code)
+  zip = c_code.zip(guess)
+  zip = zip.each do |ele|
+    ele.sort!
+  end
+
+  zip = zip.uniq!
+  difference = guess.map.each_with_index { |num, index| num == c_code[index] }
+  difference = difference.each_index.select { |i| difference[i] }
+  red_pegs = difference.count
+  intersection = guess & c_code
+  white_pegs = intersection.count - red_pegs
+  red_pegs, white_pegs = [red_pegs, white_pegs]
+end
+
+def game_over?(guess, c_code)
+    guess.eql?(c_code)
 end
